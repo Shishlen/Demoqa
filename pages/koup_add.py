e2073290e7155504a1cdcca5d5cf243080ae6f3c
Shishlen <o.shishlenin@gmail.com>
2026-03-16 22:46:35 +0300

    lection_11

---
+++ pages/koup_add.py
@@ -0,0 +1,10 @@
+from Components.components import WebElement
+from pages.base_page import BasePage
+
+class KoupAdd(BasePage):
+    def __init__(self, driver):
+        self.base_url = 'https://the-internet.herokuapp.com/'
+        super().__init__(driver, self.base_url)
+
+        self.btn_add = WebElement(driver, '#content > div > button')
+        self.btns_delete = WebElement(driver, 'button.added-manually')
