4cf832bc01c85ac490c2617f02770c8269dfbfe6
Shishlen <o.shishlenin@gmail.com>
2026-03-20 16:31:24 +0300

    lection_12

---
+++ tests/test_browser_tab.py
@@ -0,0 +1,11 @@
+import time
+from pages.browser_tab import BrowserTab
+
+def test_browser_tab(browser):
+    page_browser = BrowserTab(browser)
+    page_browser.visit()
+
+    assert len(browser.window_handles) == 1
+    page_browser.new_tab.click()
+    time.sleep(2)
+    assert len(browser.window_handles) == 2
